f668cefe2b15ad097d65dc8a198e5099f26c031c
Paul-js-hub <hesbonpoisson@gmail.com>
2023-01-22 13:57:38 +0300

    Add user's initial state to the store

--- src/redux/user/userSlice.js
+++ src/redux/user/userSlice.js
@@ -1,10 +1,19 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
+const initialState = {
+  full_name:'',
+  username:'',
+  email:'',
+  password:'',
+  pending:true,
+  error:false
+}
+
 const userSlice = createSlice({
   name: 'user',
-  initialState: {},
+  initialState,
   reducers: {},
   extraReducers: () => {},
 });
-
+export const userSelector = (state) => state.user;
 export default userSlice.reducer;
