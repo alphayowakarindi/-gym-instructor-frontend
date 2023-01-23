34c51ef525dd1f5c45da1767c56296857b4f90a1
GordonOtieno <otienogordon95@gmail.com>
2023-01-23 10:03:29 +0300

    Update signUp and Login component

--- src/component/pages/Login.jsx
+++ src/component/pages/Login.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import { Link } from 'react-router-dom';
+const Login =()=> {
 
-function Login() {
   return (
-    <div>
-      This is login form
+    <div className="flex w-full">
+      <div className="hidden w-2/4 bg-cover bg-center text-white md:block">
+        <div className="flex flex-col items-center justify-center w-full h-full bg-black bg-opacity-25">
+          <p className="text-5xl font-semibold px-6 text-center">Welcome Back!</p>
+          <p className="px-6 text-center">We are dedicated to your total fittness</p>
+        </div>
+      </div>
+      <div className="flex justify-center items-center w-full md:w-3/5 bg-white">
+        <div className="w-full h-min max-w-sm p-4">
+          <form className="space-y-6" action="#">
+            <h5 className="text-3xl text-gray-900 text-center font-bold">Sign In</h5>
+            <p></p>
+            <div className="flex flex-col gap-3">
+              <label className="uppercase text-xs tracking-wider font-semibold">Your username</label>
+              <input type="text" name="username" id="username" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Username" required />
+            </div>
+            <div className="flex flex-col gap-3">
+              <label className="uppercase text-xs tracking-wider font-semibold">Password</label>
+              <input type="password" name="username" id="username" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="********" required />
+            </div>
+            <button type="submit" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center ">Login</button>
+            <div className="text-sm font-medium text-gray-500">
+              Not registered?
+              {' '}
+              <Link to="/signup" className="text-blue-700 hover:underline">Create account</Link>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
--- src/component/pages/Signup.jsx
+++ src/component/pages/Signup.jsx
@@ -1,11 +1,51 @@
-import React from 'react'
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import { Link } from 'react-router-dom';
 
-function Signup() {
+const Signup=()=> {
+ 
   return (
-    <div>
-      This is signup form
+    <div className="w-full flex">
+      <div className="flex justify-center items-center w-full lg:w-3/5 bg-white">
+        <div className="w-full h-min max-w-sm p-4">
+          <form className="space-y-6" id="create_form" action="#">
+            <h5 className="text-3xl text-gray-900 text-center font-bold">Sign Up</h5>
+            <div className="flex flex-col gap-3 mt-5">
+              <p>Full Name</p>
+              <input type="username" name="username" id="username" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Full Name" required />
+            </div>
+            <div className="flex flex-col gap-3 mt-5">
+              <p>Username</p>
+              <input type="username" name="username" id="username" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Username" required />
+            </div>
+            <div className="flex flex-col gap-3 mt-5">
+              <p>Email</p>
+              <input type="email" name="username" id="username" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="eg.gymservices@gmail.com" required />
+            </div>
+            <div className="flex flex-col gap-3 mt-5">
+              <p>paassword</p>
+              <input type="password" name="username" id="username" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="*********" required />
+            </div>
+            <div className="flex flex-col gap-3 mt-5">
+              <p>Password_confirm</p>
+              <input type="password" name="username" id="username" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="*********" required />
+            </div>
+            <button type="submit"  className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Register</button>
+            <div className="text-sm font-medium text-gray-500">
+              Have an account?
+              {' '}
+              <Link to="/login" className="text-blue-700 hover:underline">Login</Link>
+            </div>
+          </form>
+        </div>
+      </div>
+      <div className="hidden w-2/5 bg-cover bg-center text-white md:block">
+        <div className="flex flex-col items-center justify-center w-full h-full bg-black bg-opacity-50">
+          <p className="text-5xl font-bold">Welcome, Friend!</p>
+          <p>We are happy to have you.</p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Signup
+export default Signup;
