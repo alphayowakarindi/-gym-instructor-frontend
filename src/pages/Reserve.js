1ac8dbbc6cbc30254513228a5dcdc71697ba22e9
GordonOtieno <otienogordon95@gmail.com>
2023-01-24 00:11:55 +0300

    Update product details and reserve components

--- src/pages/Reserve.js
+++ src/pages/Reserve.js
@@ -1,51 +1,32 @@
+import React from 'react';
 import HamburgerMenu from '../components/hamburgerMenu';
 
 const Reserve = () => (
   <>
     <HamburgerMenu />
-    <h1>Reserve Page</h1>
-    <div
-      className="d-flex justify-end"
-    >
-      <div className="w-full max-w-sm lg:max-w-lg backdrop-blur-sm bg-white/40 h-screen ">
-        <div className="flex flex-col px-14 lg:px-20 items-center justify-center h-full text-white">
-          <p className="text-4xl font-semibold">Reserve</p>
-          <form id="reserve_form">
-            <div className="form-group">
-              <select
-                id="service_id"
-                name="service_id"
-                className="form-control"
-                required
-              >
-                <option disabled value="default" hidden>
-                  Choose Events
-                </option>
-                <option>
-                  Select Service
-                </option>
-              </select>
-            </div>
-            <input
-              className="form-control"
-              type="text"
-              id="city"
-              placeholder="City"
-              required
-            />
-            <p className="form-control">
-              MMMM / dd / yyyy
-            </p>
-            <button
-              type="submit"
-              className="btn btn-primary"
-            >
-              Reserve
-            </button>
-          </form>
-        </div>
+    <div className="container d-flex my-5">
+      <div className="hero-message d-flex align-items-center me-5 h3">
+        You are about to reserve this service.
       </div>
+      <form action="/action_page.php" className="d-flex flex-column align-items-center">
+        <p className="h3 text-bold pb-2">Reservation Form</p>
+        <input type="text" id="user" name="user" value="" className="form-control p-2 m-2" required hidden />
+        <input type="text" id="service_id" name="service_id" value="" className="form-control p-2 m-2" required hidden />
+        <select className="m-2 p-2 w-100" id="exampleFormControlSelect1" required>
+          <option value="" disabled selected hidden>
+            Select a service
+          </option>
+          <option>Option 1</option>
+          <option>Option 2</option>
+          <option>Option 3</option>
+        </select>
+        <input type="text" id="start-date" name="city" value="" placeholder="Your City" className="form-control p-2 m-2" required />
+        <input type="date" id="start-date" name="start-date" value="" placeholder="Your Start Date" className="form-control p-2 m-2" required />
+        <input type="submit" value="Reserve" className="form-control m-2 btn btn-secondary" />
+      </form>
+
     </div>
+
   </>
 
 );
--- src/pages/ServiceDetails.js
+++ src/pages/ServiceDetails.js
@@ -1,41 +1,70 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { AiOutlineSchedule } from 'react-icons/ai';
+import { FaAngleDoubleRight, FaAngleLeft } from 'react-icons/fa';
 import image from './images/yoga.jpg';
+import HamburgerMenu from '../components/hamburgerMenu';
+import icon from './images/icon.png';
 
 function ServiceDetails() {
   return (
-    <div className="container d-flex justify-content-between ">
-      <div className="image-container">
-        <img src={image} alt="wel" />
-      </div>
+    <>
+      <HamburgerMenu />
+      <div className="container service-details align-items-center justify-content-center p-5">
+        <div className="raw d-flex py-5">
+          <div className="col-lg-6 col-sm-3 d-flex flex-column justify-content-between">
+            <div className="images-display">
+              <img src={image} alt="wel" className="img-thumbnail" />
+            </div>
+            <div>
+              <button type="button" className="explore-btn">
+                <FaAngleLeft size={25} className="text-white" />
+                See More
+              </button>
+            </div>
+          </div>
 
-      <div className="service-info ms-5">
-        <div className="fd-flex justify-content-between px-2 py-2 gap-3 bg-light">
-          <span>Name</span>
-          <span>Yoga</span>
+          <div className="col-lg-6 col-sm-3">
+            <form className="d-flex flex-column">
+              <div className="d-flex justify-content-between">
+                <span className="h3">Yoga</span>
+              </div>
+              <div className="d-flex justify-content-between bg-light p-2 my-1">
+                <span>Class Time</span>
+                <span>5:00pm</span>
+              </div>
+              <div className="d-flex justify-content-between p-2 my-1">
+                <span>Amount Payable</span>
+                <span>$500</span>
+              </div>
+              <div className="d-flex justify-content-between border bg-gray-100 px-2 py-2">
+                <span>Stay Duration</span>
+                <span>3 Months</span>
+              </div>
+              <div className="d-flex align-items-end flex-column">
+                <span className="p-3">
+                  Discover more
+                  {' '}
+                  <FaAngleDoubleRight />
+                </span>
+                <div className="spining-img">
+                  <img src={icon} alt="spinning" className="img-thumbnail" />
+                </div>
+                <Link to="/reserve">
+                  <button
+                    type="button"
+                    className="reserve"
+                  >
+                    <AiOutlineSchedule size={25} className="text-white" />
+                    <span className="mx-1 text-white">Reserve Service</span>
+                  </button>
+                </Link>
+              </div>
+            </form>
+          </div>
         </div>
-        <div className="d-flex justify-content-between border bg-gray-100 px-2 py-2">
-          <span>Category</span>
-          <span>Yoga</span>
-        </div>
-        <div className="d-flex justify-content-between border px-2 py-2">
-          <span>Price</span>
-          <span>$500</span>
-        </div>
-        <div className="d-flex justify-content-between border bg-gray-100 px-2 py-2">
-          <span>Duration</span>
-          <span>3 Months</span>
-        </div>
-        <Link to="/reserve">
-          <button
-            type="button"
-            className=""
-          >
-            Reserve
-          </button>
-        </Link>
       </div>
-    </div>
+    </>
   );
 }
 
